130865b742ea45730008dc29f23efd6ce7d2ec59
harshavardhan1905 <podetiharshavardhan@gmail.com>
2026-01-17 22:14:21 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -54,6 +54,8 @@ const HeroSection: React.FC = () => {
               <div className="absolute inset-0 bg-blue-600/10 dark:bg-blue-400/10 rounded-full animate-pulse-slow"></div>
               <div className="absolute inset-4 bg-gradient-to-br from-blue-400 to-teal-1000 rounded-full overflow-hidden shadow-xl">
                <p>Hello img</p>
+     
+ <p className='w-full h-full object-cover'> <AdvancedImage cldImg={img}/></p> 
 
               </div>
             </div>
